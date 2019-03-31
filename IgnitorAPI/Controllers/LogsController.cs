2b4b87bf104aaba3f73bd371ec227b4c9bc6424e
Chris OBeirne <chris.obeirne@onyxdigital.com.au>
2019-03-31 21:14:24 +1100

    Add Test Project and Cleanup Log Controller

--- IgnitorAPI/Controllers/LogsController.cs
+++ IgnitorAPI/Controllers/LogsController.cs
@@ -1,12 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using NLog;
 using NLog.Targets;
-
-// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+using System;
+using System.Collections.Generic;
 
 namespace IgnitorAPI.Controllers
 {
